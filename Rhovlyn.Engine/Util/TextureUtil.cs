9d409dc2b3ed32443037a15aa12657d67b1ab5f1
David Barnett <david@barnett.net.nz>
2014-06-27 12:38:43 +1200

    Optimised EncapsulateTexture * Changed search algorithm

--- Rhovlyn.Engine/Util/TextureUtil.cs
+++ Rhovlyn.Engine/Util/TextureUtil.cs
@@ -12,49 +12,105 @@ namespace Rhovlyn.Engine.Util
 		/// <summary>
 		/// The .
 		/// </summary>
-		public static Rectangle EncapsulateTexture(Texture2D Texture, Rectangle? InArea)
+		public static Rectangle EncapsulateTexture(Texture2D texture, Rectangle? inArea, byte alpha = 250)
 		{
 
-			if (InArea == null)
+			if (inArea == null)
 			{
-				InArea = new Rectangle(0, 0, Texture.Width, Texture.Height);
+				inArea = new Rectangle(0, 0, texture.Width, texture.Height);
 			}
-			var output = (Rectangle)InArea;
+			var output = (Rectangle)inArea;
 			var pixels = new Color[output.Width * output.Height];
-			Texture.GetData<Color>(0, output, pixels, 0, pixels.Length);
+			texture.GetData<Color>(0, output, pixels, 0, pixels.Length);
 
 			int top = output.Height, bot = 0, left = output.Width, right = 0;
 
+			bool found = false;
+			//Check in horizontal slices
 			for (int y = 0; y < output.Height; y++)
 			{
 				for (int x = 0; x < output.Width; x++)
 				{
 					var pixel = pixels[y * output.Width + x];
-					if (pixel.A > 128)
+					if (pixel.A > alpha)
 					{
-						if (x < left)
-						{
-							left = x;
-						}
-						if (x > right)
-						{
-							right = x;
-						}
+						//Found the closest pixel to the top
+						top = y;
+						//Best guess for 1st pixel to the left
+						left = x;
 
-						if (y < top)
-						{
-							top = y;
-						}
-						if (y > bot)
-						{
-							bot = y;
-						}
+						found = true;
+						break;
 					}
 				}
+				if (found)
+					break;
 			}
-			output = new Rectangle(output.X + left, output.Y + top, right - left + 1, bot - top + 1);
 
+			found = false;
+			//Check in horizontal slices
+			for (int y = output.Height - 1; y > top; y--)
+			{
+				for (int x = output.Width - 1; x > left; x--)
+				{
+					var pixel = pixels[y * output.Width + x];
+					if (pixel.A > alpha)
+					{
+						//Found the closest pixel to the bottom
+						bot = y;
+						//Best guess for last pixel to the right
+						right = x;
+						found = true;
+						break;
+					}
+				}
+				if (found)
+					break;
+			}
+
+			found = false;
+			//Search between 0 and the best guess
+			//Check in vertical slices
+			for (int x = 0; x < left; x++)
+			{
+				//Shorten search to between top and bot
+				for (int y = top; y < bot; y++)
+				{
+					var pixel = pixels[y * output.Width + x];
+					if (pixel.A > alpha)
+					{
+						//Found the closest pixel to the left
+						left = x;
+						found = true;
+						break;
+					}
+				}
+				if (found)
+					break;
+			}
+
+			found = false;
+			//Check in vertical slices
+			for (int x = output.Width - 1; x > right; x--)
+			{
+				for (int y = top; y < bot; y++)
+				{
+					var pixel = pixels[y * output.Width + x];
+					if (pixel.A > alpha)
+					{
+						//Found the closest pixel to the right
+						right = x;
+						found = true;
+						break;
+					}
+				}
+				if (found)
+					break;
+			}
+
+			output = new Rectangle(output.X + left, output.Y + top, right - left + 1, bot - top + 1);
 			Console.WriteLine(output);
+
 			return output;
 		}
 
